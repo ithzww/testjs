c301741d2900f94362c64475dfa9b2fd010a2a8f
Teeraphong Vechatrakoolphaisan <56422858+ithzww@users.noreply.github.com>
2021-11-04 12:11:09 +0700

    Update my.js

--- my.js
+++ my.js
@@ -9,4 +9,27 @@ function testGS() {
     });
 }
 
-document.getElementById("btn").addEventListener("click",testGS);
+function addGS() {
+  const url =
+    "https://script.google.com/macros/s/AKfycbxd6ko-Gabnx5CUa5aLs-MDDjnM6YDYfTGX2U5GJuKkW6y9vktMGqIaK03nlXhVQIUB/exec";
+
+  fetch(url,{
+      method: 'POST',
+      mode: 'no-cors',
+      cache: 'no-cache',
+      //credentials: 'omit',
+      headers: {'Content-Type': 'application/json'},
+      redirect: 'follow',
+      //referrerPolicy: 'no-referrer',
+      body: JSON.stringify({name:"Jhon"})
+
+
+
+
+
+  });
+}
+
+document.getElementById("btn2").addEventListener("click", addGS);
+
+document.getElementById("btn").addEventListener("click", testGS);
