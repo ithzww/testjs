af345152c0ba994b4a3947077dafc9e0b8fd9880
Teeraphong Vechatrakoolphaisan <56422858+ithzww@users.noreply.github.com>
2021-11-04 11:51:05 +0700

    Update my.js

--- my.js
+++ my.js
@@ -1,6 +1,6 @@
 function testGS() {
   const url =
-    "https://script.google.com/macros/s/AKfycbwN5ge8IidT50y1WT4p1lomKhjX9zVHxcdB5NbrH9RxH6j89Phpx2qRRiLs2GVNpTHm/exec";
+    "https://script.google.com/macros/s/AKfycbxd6ko-Gabnx5CUa5aLs-MDDjnM6YDYfTGX2U5GJuKkW6y9vktMGqIaK03nlXhVQIUB/exec";
 
   fetch(url)
     .then((d) => d.json())
